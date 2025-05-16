4a986822d59ac3cd1b55d45489763e672b8c6b31
Museyib Huseynov <museyib.huseynov.dev@gmail.com>
2025-05-16 16:31:50 +0400

    update

--- db/view.sql
+++ db/view.sql
@@ -40,7 +40,7 @@ SELECT
   NULLIF(
     SUM(oil_ton_calc) OVER (PARTITION BY f.id, rd.report_date), 0
   ) AS oil_ton_allocated,
-  ISNULL(oil_ton_calc * (24 - dwp.well_uptime_hours) / dwp.well_uptime_hours, 0) AS oil_loss_ton_wellTest,
+  ISNULL(oil_ton_calc * (24 - dwp.well_uptime_hours) / NULLIF(dwp.well_uptime_hours, 0), 0) AS oil_loss_ton_wellTest,
   water_ton_calc AS water_ton,
   ROUND(((gwt.total_gas / 24) * dwp.well_uptime_hours), 0) AS total_gas,
   ROUND((gwt.gaslift_gas / 24) * dwp.well_uptime_hours, 0) AS gaslift_gas,
